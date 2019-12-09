94c12701f8eb1c6c419b5913d14699aec475aa75
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-12-09 17:42:58 +0800

    add api methods

--- src/api.js
+++ src/api.js
@@ -13,3 +13,28 @@ export const search = async (q, offset = 0) => {
   const rv = await instance.get("/search", { params: { q, offset } });
   return rv.data;
 };
+
+export const enqueue = track => instance.post(`/track/${track.id}/enqueue`);
+
+export const getCurrentTrack = async () => {
+  const rv = await instance.get("/track/current");
+  return rv.data;
+};
+
+export const getAlbumTracks = async (album, offset = 0) => {
+  const params = { offset };
+  const rv = await instance.get(`/album/${album.id}/tracks`, { params });
+  return rv.data;
+};
+
+export const getArtistDetails = async (artist, offset = 0) => {
+  const params = { offset };
+  const rv = await instance.get(`/artist/${artist.id}/details`, { params });
+  return rv.data;
+};
+
+export const getPlaylistTracks = async (playlist, offset = 0) => {
+  const params = { offset };
+  const rv = await instance.get(`/playlist/${playlist.id}/tracks`, { params });
+  return rv.data;
+};
