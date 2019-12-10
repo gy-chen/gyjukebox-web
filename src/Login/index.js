0214e62acfef7028fba4e83af10ff5c3f5985d6e
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-12-10 20:58:28 +0800

    update Login style

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { fetchTokenFromUrl } from "./utils";
+import style from "./Login.module.css";
 
 const LOGIN_URL = process.env.REACT_APP_LOGIN_URL;
 
@@ -26,7 +27,15 @@ class Login extends React.Component {
 
   render() {
     return (
-      <button onClick={this._onLoginButtonClick}>Login to Continue</button>
+      <div className={style.container}>
+        <h1>GYJUKEBOX</h1>
+        <button
+          onClick={this._onLoginButtonClick}
+          className={style.loginButton}
+        >
+          Login
+        </button>
+      </div>
     );
   }
 }
